3b48a1491e6a271a4ce59398b908a21901ecd5fb
mahuichao <mahuichao@dip-ai.com>
2019-10-28 16:12:41 +0800

    .

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ from models.loss import build_loss
 from models.metrics import build_iou,mean_iou
 from keras.utils import multi_gpu_model
 
-
+model.load_weights('./tf/single0929.hdf5')
 
 #%%
 adam = Adam(1e-4)
@@ -49,7 +49,7 @@ from tool.generator import Generator
 
 #%%
 import config 
-train_dir = [config.MIWI_2018_TRAIN_LABEL_DIR,
+train_dir = [#config.MIWI_2018_TRAIN_LABEL_DIR,
              config.DIP_TRAIN_LABEL_DIR_TEXT_ZIXUAN,
              config.DIP_TRAIN_LABEL_DIR_TEXT_ZIXUAN2,
              config.DIP_TEST_LABEL_DIR_TEXT_ZIXUAN3,
@@ -72,7 +72,7 @@ train_dir = [config.MIWI_2018_TRAIN_LABEL_DIR,
              config.DIP_TEST_LABEL_DIR_TEXT_ZIXUAN4,
              ]
 
-test_dir = [config.MIWI_2018_TEST_LABEL_DIR,
+test_dir = [#config.MIWI_2018_TEST_LABEL_DIR,
             config.DIP_TRAIN_LABEL_DIR_TEXT_ZIXUAN,
             config.DIP_TRAIN_LABEL_DIR_TEXT_ZIXUAN2,
             config.DIP_TEST_LABEL_DIR_TEXT_ZIXUAN3,
@@ -89,7 +89,7 @@ gen_train = Generator(train_dir,batch_size = batch_size ,istraining=True,
                         clip = True,angle = 10,max_size = None)
 
 #%%
-gen_test = Generator(test_dir,batch_size = batch_size ,istraining = False,
+gen_test = Generator(test_dir,batch_size = 1 ,istraining = False,
                     num_classes=num_class,mirror = False,reshape=None,
                     trans_color = False,trans_gray=False,scale = False,
                     clip = False,angle = None,max_size = 1280)
@@ -116,7 +116,7 @@ res = multi_model.fit_generator(gen_train,
                           validation_data=gen_test,
                           validation_steps =gen_test.num_samples()//batch_size ,
                           verbose=1,
-                          initial_epoch=0,
+                          initial_epoch=150,
                           workers=4,
                           use_multiprocessing=False,
                           max_queue_size=64,
